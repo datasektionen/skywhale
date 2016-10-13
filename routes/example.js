62d3d73fa7592f7d96798152c55adf55a5992ddd
jsimo <jsimo@kth.se>
2016-10-13 21:53:21 +0200

    first test api endpoint

--- routes/example.js
+++ routes/example.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router  = express.Router();
+var models = require('../models');
 
-router.use('/', function(req, res) {
+router.use('/teapot', function(req, res) {
   res.status(418);
   res.send("hello world from the teapot");
+});
+
+router.get('/sm', function(req, res) {
+  models.SM.findAll({}).then(function(sm) {
+    res.send(sm);
+  });
 });
 
 module.exports = router;
