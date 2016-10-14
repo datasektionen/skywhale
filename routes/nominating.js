2057e5d5dacf828b4384176a09925979e063f149
jsimo <jsimo@kth.se>
2016-10-14 15:19:02 +0200

    added todo for nominate endpoint

--- routes/nominating.js
+++ routes/nominating.js
@@ -10,6 +10,13 @@ router.post('/nominatefreetext',function(req, res) {
 
 router.get('/nominate', function(req, res) {
 	//kthid
+	//1. Check if the given kthid has already been nominated for that post.
+	// 	 (if already nominated return error)
+	//2. Get user data from zfinger
+	//3. Add nomination to the database
+	//4. Send email to nominated person
+	//   (with the correct name for sm, date and such)
+	//5. Return success!
 	mail.sendNewNomination("Lili Du", "lilid", ["ordförande"], "17:e maj", "Val-SM").then(function(bla) {
 		res.send(bla);
 	}).catch(function(err) {
