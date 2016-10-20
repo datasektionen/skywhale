5735bc13a2110b596dafebf5bd4a5eb13ebf4f60
Jonas Dahl <jonas@jdahl.se>
2016-10-20 14:24:32 +0200

    Partly fixes #7 by adding edit/remove links on user page, reachable by clicking the name on front page

--- resources/views/show-person.blade.php
+++ resources/views/show-person.blade.php
@@ -4,23 +4,35 @@
 
 @section('content')
 
-<div class="crop big" style="width: 200px; height: 200px;background-size: 200px;background-image: url(https://zfinger.datasektionen.se/user/{{ $user->kth_username }}/image/500);"></div>
-<table>
-	<tr>
-		<td>
-			Namn:
-		</td>
-		<td>
-			{{ $user->name }}
-		</td>
-	</tr>
-	<tr>
-		<td>
-			E-postadress:
-		</td>
-		<td>
-			{{ $user->kth_username }}@kth.se
-		</td>
-	</tr>
-</table>
+<div class="center">
+	<div class="crop big" style="width: 200px; height: 200px;background-size: 200px;background-image: url(https://zfinger.datasektionen.se/user/{{ $user->kth_username }}/image/500);"></div>
+	<table>
+		<tr>
+			<td>
+				Namn:
+			</td>
+			<td>
+				{{ $user->name }}
+			</td>
+		</tr>
+		<tr>
+			<td>
+				E-postadress:
+			</td>
+			<td>
+				{{ $user->kth_username }}@kth.se
+			</td>
+		</tr>
+		@if (Auth::check() && Auth::user()->isAdmin())
+			<tr>
+				<td>
+					Administrera:
+				</td>
+				<td>
+					<a href="/admin/persons/edit/{{ $user->id }}">Ändra</a> | <a href="/admin/persons/remove/{{ $user->id }}">Ta bort</a>
+				</td>
+			</tr>
+		@endif
+	</table>
+</div>
 @endsection
