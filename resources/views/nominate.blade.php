e4f9de9f669ec28cbab16df632fd32f6474c620d
Jonas Dahl <jonas@jdahl.se>
2016-10-20 13:56:59 +0200

    Fixes #6 - added description to nominate field

--- resources/views/nominate.blade.php
+++ resources/views/nominate.blade.php
@@ -39,7 +39,8 @@ $(document).ready(function () {
 <div class="form">
     <div class="form-entry">
         <span class="description">
-            Vem vill du nominera?
+            Vem vill du nominera?<br>
+            <span class="desc">Börja skriv ett namn så kommer en lista där du kan välja personer. Du kan också skriva manuellt. Kan du då inte allas KTH-mejl utantill? Kolla <a href="https://zfinger.datasektionen.se">Z-Finger.</a></span>
         </span>
         <div class="input">
             {!! Form::text('name', NULL, array('placeholder' => 'Namn', 'id' => 'name')) !!}
