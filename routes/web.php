41b4f71ee27fb553acc864b109f9d385b9acffbc
Jonas Dahl <jonas@jdahl.se>
2016-10-24 09:23:57 +0200

    Requiring https for all routes, possibly fixing #2

--- routes/web.php
+++ routes/web.php
@@ -18,50 +18,53 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-/**
- * Main routes.
- */
-Route::get ('/', 'Controller@getWelcome');
-Route::get('person/{id}', 'Controller@getPerson');
+// Define all routes as https
+Route::group(array('https'), function() {
+	/**
+	 * Main routes.
+	 */
+	Route::get ('/', 'Controller@getWelcome');
+	Route::get('person/{id}', 'Controller@getPerson');
 
-Route::get ('nominate', 'NominationController@getNominate');
-Route::get ('nomination/answer', 'NominationController@getNominationAnswer')		->middleware('auth');
-Route::get ('nomination/answer/accept/{uuid}', 'NominationController@getAccept')	->middleware('auth');
-Route::get ('nomination/answer/decline/{uuid}', 'NominationController@getDecline')	->middleware('auth');
-Route::get ('nomination/answer/regret/{uuid}', 'NominationController@getRegret')	->middleware('auth');
-Route::post('nominate', 'NominationController@postNominate');
+	Route::get ('nominate', 'NominationController@getNominate');
+	Route::get ('nomination/answer', 'NominationController@getNominationAnswer')		->middleware('auth');
+	Route::get ('nomination/answer/accept/{uuid}', 'NominationController@getAccept')	->middleware('auth');
+	Route::get ('nomination/answer/decline/{uuid}', 'NominationController@getDecline')	->middleware('auth');
+	Route::get ('nomination/answer/regret/{uuid}', 'NominationController@getRegret')	->middleware('auth');
+	Route::post('nominate', 'NominationController@postNominate');
 
 
-/**
- * Admin routes.
- */
-Route::get('admin', 'Admin\AdminController@getIndex')								->middleware('admin');
+	/**
+	 * Admin routes.
+	 */
+	Route::get('admin', 'Admin\AdminController@getIndex')								->middleware('admin');
 
-Route::get ('admin/positions', 'Admin\PositionAdminController@getShow')				->middleware('admin');
+	Route::get ('admin/positions', 'Admin\PositionAdminController@getShow')				->middleware('admin');
 
-Route::get ('admin/elections', 'Admin\ElectionAdminController@getShow')				->middleware('admin');
-Route::get ('admin/elections/new', 'Admin\ElectionAdminController@getNew')			->middleware('admin');
-Route::post('admin/elections/new', 'Admin\ElectionAdminController@postNew')			->middleware('admin');
-Route::get ('admin/elections/edit/{id}', 'Admin\ElectionAdminController@getEdit')	->middleware('admin');
-Route::post('admin/elections/edit/{id}', 'Admin\ElectionAdminController@postEdit')	->middleware('admin');
-Route::get ('admin/elections/remove-nomination/{uuid}', 'Admin\ElectionAdminController@getRemoveNomination')->middleware('admin');
-Route::get ('admin/elections/remove-nomination-sure/{uuid}', 'Admin\ElectionAdminController@getRemoveNominationSure')->middleware('admin');
-Route::get ('admin/elections/remove/{id}', 'Admin\ElectionAdminController@getRemove')->middleware('admin');
-Route::get ('admin/elections/remove-confirmed/{id}', 'Admin\ElectionAdminController@getRemoveConfirmed')->middleware('admin');
-Route::get ('admin/elections/edit-nomination/{uuid}', 'Admin\ElectionAdminController@getEditNomination')->middleware('admin');
-Route::post('admin/elections/edit-nomination/{uuid}', 'Admin\ElectionAdminController@postEditNomination')->middleware('admin');
+	Route::get ('admin/elections', 'Admin\ElectionAdminController@getShow')				->middleware('admin');
+	Route::get ('admin/elections/new', 'Admin\ElectionAdminController@getNew')			->middleware('admin');
+	Route::post('admin/elections/new', 'Admin\ElectionAdminController@postNew')			->middleware('admin');
+	Route::get ('admin/elections/edit/{id}', 'Admin\ElectionAdminController@getEdit')	->middleware('admin');
+	Route::post('admin/elections/edit/{id}', 'Admin\ElectionAdminController@postEdit')	->middleware('admin');
+	Route::get ('admin/elections/remove-nomination/{uuid}', 'Admin\ElectionAdminController@getRemoveNomination')->middleware('admin');
+	Route::get ('admin/elections/remove-nomination-sure/{uuid}', 'Admin\ElectionAdminController@getRemoveNominationSure')->middleware('admin');
+	Route::get ('admin/elections/remove/{id}', 'Admin\ElectionAdminController@getRemove')->middleware('admin');
+	Route::get ('admin/elections/remove-confirmed/{id}', 'Admin\ElectionAdminController@getRemoveConfirmed')->middleware('admin');
+	Route::get ('admin/elections/edit-nomination/{uuid}', 'Admin\ElectionAdminController@getEditNomination')->middleware('admin');
+	Route::post('admin/elections/edit-nomination/{uuid}', 'Admin\ElectionAdminController@postEditNomination')->middleware('admin');
 
-Route::get ('admin/persons', 'Admin\PersonAdminController@getShow')					->middleware('admin');
-Route::post('admin/persons/merge', 'Admin\PersonAdminController@postMerge')			->middleware('admin');
-Route::get ('admin/persons/merge', 'Admin\PersonAdminController@getMerge')			->middleware('admin');
-Route::post('admin/persons/merge-final', 'Admin\PersonAdminController@postMergeFinal')->middleware('admin');
-Route::get ('admin/persons/new', 'Admin\PersonAdminController@getNew')				->middleware('admin');
-Route::post('admin/persons/new', 'Admin\PersonAdminController@postNew')				->middleware('admin');
-Route::get ('admin/persons/edit/{id}', 'Admin\PersonAdminController@getEdit')		->middleware('admin');
-Route::post('admin/persons/edit/{id}', 'Admin\PersonAdminController@postEdit')		->middleware('admin');
-Route::get ('admin/persons/remove/{id}', 'Admin\PersonAdminController@getRemove')   ->middleware('admin');
-Route::get ('admin/persons/remove-confirmed/{id}', 'Admin\PersonAdminController@getRemoveConfirmed')->middleware('admin');
+	Route::get ('admin/persons', 'Admin\PersonAdminController@getShow')					->middleware('admin');
+	Route::post('admin/persons/merge', 'Admin\PersonAdminController@postMerge')			->middleware('admin');
+	Route::get ('admin/persons/merge', 'Admin\PersonAdminController@getMerge')			->middleware('admin');
+	Route::post('admin/persons/merge-final', 'Admin\PersonAdminController@postMergeFinal')->middleware('admin');
+	Route::get ('admin/persons/new', 'Admin\PersonAdminController@getNew')				->middleware('admin');
+	Route::post('admin/persons/new', 'Admin\PersonAdminController@postNew')				->middleware('admin');
+	Route::get ('admin/persons/edit/{id}', 'Admin\PersonAdminController@getEdit')		->middleware('admin');
+	Route::post('admin/persons/edit/{id}', 'Admin\PersonAdminController@postEdit')		->middleware('admin');
+	Route::get ('admin/persons/remove/{id}', 'Admin\PersonAdminController@getRemove')   ->middleware('admin');
+	Route::get ('admin/persons/remove-confirmed/{id}', 'Admin\PersonAdminController@getRemoveConfirmed')->middleware('admin');
 
-Route::get ('logout', 'AuthController@getLogout')									->middleware('auth');
-Route::get ('login', 'AuthController@getLogin')										->middleware('guest');
-Route::get ('login-complete/{token}', 'AuthController@getLoginComplete');
+	Route::get ('logout', 'AuthController@getLogout')									->middleware('auth');
+	Route::get ('login', 'AuthController@getLogin')										->middleware('guest');
+	Route::get ('login-complete/{token}', 'AuthController@getLoginComplete');
+});
