3eb8f5610e423890594b9e6706069c9015b1d818
Jonas Dahl <jonas@jdahl.se>
2016-10-30 23:52:06 +0100

    Increased the number of RSS entries a bit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -48,7 +48,7 @@ class Controller extends BaseController {
 	public function getRss() {
 		return response(view('rss')
 			->with('positions', Position::allKey())
-			->with('events', Event::orderBy('id', 'DESC')->limit(10)->get()))
+			->with('events', Event::orderBy('id', 'DESC')->limit(100)->get()))
 			->header('Content-Type', 'application/rss+xml; charset=utf-8');
 	}
 }
