0a5d50398d2e1f8531a31b766cd9bdccaa73da4b
Jonas Dahl <jonas@jdahl.se>
2016-11-24 21:16:06 +0100

    Removed deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp       = require('gulp');
 var elixir     = require('laravel-elixir');
-var browserify = require('browserify');
-var source     = require('vinyl-source-stream');
 var reactify   = require('reactify');
 
 elixir((mix) => {
