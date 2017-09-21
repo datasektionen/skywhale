2dd60af9fd3d44096ee6bcc0b325ead9a7a9aa91
Jonas Dahl <jonas@jdahl.se>
2017-09-21 22:43:12 +0200

    Added support for highres favicon :)

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,7 +16,23 @@
     <link href="/css/app.css" rel="stylesheet" type="text/css">
     <link href="/css/jquery-ui.min.css" rel="stylesheet" type="text/css">
 
-    <meta name="theme-color" content="#06c" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/images/logos/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/logos/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/logos/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/logos/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/logos/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/logos/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/logos/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/logos/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/logos/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/images/logos/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/logos/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/logos/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/logos/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#06c">
+    <meta name="msapplication-TileImage" content="/images/logos/ms-icon-144x144.png">
+    <meta name="theme-color" content="#06c">
     @yield('head-extra')
     <script type="text/javascript">
 
